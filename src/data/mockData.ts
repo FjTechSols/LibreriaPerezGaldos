e0328ed189a58f8eab503502e0518f849b844b55
FjTechSols <fjtechsols@gmail.com>
2025-10-01 10:16:19 +0200

    corrections admin dashboard

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -175,8 +175,8 @@ export const mockCompanyInfo: CompanyInfo = {
   name: 'Perez Galdos S.L.',
   address: 'Calle Hortaleza 5, 28004 Madrid, España',
   phone: '+34 91 531 26 40',
-  email: 'info@libreriaonline.com',
+  email: 'librería@perezgaldos.com',
   website: 'www.perezgaldos.es',
-  taxId: 'B12345678',
+  taxId: '000000000',
   logo: 'https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=200'
 };
